3aa4f4089bb4f7a373db41e91458eb0f47539de3
Ahmad Ajmi <ahmadajmi@outlook.com>
2014-03-06 16:17:33 +0200

    Init Commit

---
+++ Gruntfile.js
@@ -0,0 +1,33 @@
+module.exports = function(grunt){
+
+	require("matchdep").filterDev("grunt-*").forEach(grunt.loadNpmTasks);
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+		watch: {
+		    css: {
+		        files: ['css/**/*.scss'],
+		        tasks: ['buildcss']
+		    }
+		}
+		,
+		cssmin: {
+		    build: {
+		        src: 'css/main.css',
+		        dest: 'css/main.min.css'
+		    }
+		}
+		,
+		sass: {
+		    build: {
+		        files: {
+		            'css/main.css': 'css/main.scss'
+		        }
+		    }
+		}
+    });
+
+    grunt.registerTask('default', []);
+    grunt.registerTask('buildcss',  ['sass', 'cssmin']);
+
+};
---
+++ README.md
@@ -0,0 +1,1 @@
+A very simple grid system inspired by Foundation.
